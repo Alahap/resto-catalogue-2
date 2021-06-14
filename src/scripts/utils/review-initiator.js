a79042e336b1fc42d0096cf6c1a5ab56d2275bb0
Alahap7tohang <66238022+Alahap@users.noreply.github.com>
2021-06-14 09:29:19 +0700

    Add files via upload

---
+++ src/scripts/utils/review-initiator.js
@@ -0,0 +1,27 @@
+// /* eslint-disable linebreak-style */
+// /* eslint-disable no-underscore-dangle */
+
+// const AddReviewInitiator = {
+//   async init({ addReviewContainer, review }) {
+//     this._addReviewContainer = addReviewContainer;
+//     this._review = review;
+
+//     await this._renderReview();
+//   },
+
+//   _renderReview(name, review) {
+//       const addReviewContainer = document.querySelector('#addReview-container');
+//       const date = new Date().toLocaleDateString('id-ID', { year: 'numeric', month: 'long', day: 'numeric'}),
+
+//       const review = `
+//         <div class="reviewer-card">
+//             <p class="reviewer-name">${name}</p>
+//             <p class="reviewer-comment">${review}</p>
+//             <p class="reviewer-date">${date}</p>
+//         </div>
+//       `;
+//       addReviewContainer.innerHTML += dataReview;
+//   },
+// };
+
+// export default AddReviewInitiator;
