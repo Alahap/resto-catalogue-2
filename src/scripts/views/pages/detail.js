3585d28df63541a2518178398d73cc0552bf9dc4
Alahap7tohang <66238022+Alahap@users.noreply.github.com>
2021-06-26 10:02:31 +0700

    Add files via upload

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,75 @@
+import UrlParser from '../../routes/url-parser';
+import RestaurantSource from '../../data/restaurant-source';
+import { createRestaurantDetailTemplate } from '../templates/template-creator';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
+
+const Detail = {
+  async render() {
+    return `
+    <div class="content">
+      <div id="restaurant" class="restaurant"></div>
+      <div id="likeButtonContainer"></div>
+    </div>
+    `;
+  },
+
+  async afterRender() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const data = await RestaurantSource.detailPage(url.id);
+    const restaurantContainer = document.querySelector('#restaurant');
+    restaurantContainer.innerHTML = createRestaurantDetailTemplate(data.restaurant);
+
+    document.querySelector('.btnSubmit').addEventListener('click', async (e) => {
+      e.preventDefault();
+      const inputReview = document.querySelector('.inputReview');
+      const form = document.querySelector('form');
+      const inputName = document.querySelector('.inputName');
+
+      const reviewData = {
+        id: data.restaurant.id,
+        name: inputName.value,
+        review: inputReview.value,
+      };
+
+      if (inputName.value === '') {
+        alert('Name cannot be empty!');
+      } else if (inputReview.value === '') {
+        alert('Review cannot be empty!');
+      } else {
+        await RestaurantSource.addReview(reviewData);
+        form.reset();
+        this._renderReview(reviewData.name, reviewData.review);
+      }
+    }),
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: url.id,
+        pictureId: data.restaurant.pictureId,
+        name: data.restaurant.name,
+        description: data.restaurant.description,
+        rating: data.restaurant.rating,
+        city: data.restaurant.city
+      },
+    });
+  },
+
+  _renderReview(name, review) {
+    const reviewContainer = document.querySelector('.review-container');
+    const date = new Date().toLocaleDateString('id-ID', { year: 'numeric', month: 'long', day: 'numeric' });
+
+    const dataReview = `
+      <div class="review-card">
+        <p class="review-name">${name}</p>
+        <p class="review-comment">${review}</p>
+        <p class="review-date">${date}</p>
+      </div>
+    `;
+
+    reviewContainer.innerHTML += dataReview;
+  },
+
+};
+
+export default Detail;
---
+++ src/scripts/views/templates/template-creator.js
@@ -0,0 +1,78 @@
+import CONFIG from '../../globals/config';
+
+const createRestaurantDetailTemplate = (restaurant) => `
+  <h2 class="restaurant_name">${restaurant.name}</h2>
+  <img tabindex="4" class="restaurant__image" src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}" alt="${restaurant.name}"/>
+  <div class="restaurant__info">
+    <h4><span class="material-icons">place</span>${restaurant.city}</h4>
+    <p>${restaurant.address}</p>
+    <h4><span class="material-icons">star_rate</span>${restaurant.rating}</h4>
+    <div class="info_category">
+      <h3>Category : ${restaurant.categories.map((category) => category.name).join(', ')}</h3>
+      <h3>Foods</h3> ${restaurant.menus.foods.map((food) => food.name).join(', ')}
+      <h3>Drinks</h3> ${restaurant.menus.drinks.map((drink) => drink.name).join(', ')}
+    </div>
+  </div>
+  <div class="restaurant_description">
+    <h3>Overview</h3>
+    <p>${restaurant.description}</p>
+    <h3>Customer Reviews<h3>
+    <div class="review-container">
+    ${restaurant.customerReviews.map((review) => `
+      <div class="review-card">
+        <p class="review-name">${review.name}</p>
+        <p class="review-comment">${review.review}</p>
+        <p class="review-date">${review.date}</p>
+      </div>
+    `).join('')}
+    </div>
+  </div>
+
+  <div id="addReviewContainer" class="detail-form">
+    <form>
+      <label for="inputName">Name</label>
+      <input type="text" name="nama" class="inputName" id="inputName" placeholder="Enter Name">
+      <label for="inputReview">Review</label>
+      <textarea name="review" class="inputReview" id="inputReview" placeholder="Enter Review"></textarea>
+      <button type="submit" class="btnSubmit">Send</button>
+    </form>
+  </div>
+
+  </div>
+`;
+
+const createRestaurantItemTemplate = (restaurant) => `
+  <div class="restaurant-item">
+    <div class="restaurant-item__header">
+        <img class="restaurant-item__header__image" alt="${restaurant.name}"
+            src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}">
+        <div class="restaurant-item__header__rating">
+            <p>⭐️<span>${restaurant.rating}</span></p>
+        </div>
+    </div>
+    <div class="restaurant-item__content">
+        <div class="link_name"><a tabindex="0" href="${`/#/detail/${restaurant.id}`}">${restaurant.name}</a></div>
+        <h4><span class="material-icons">place</span>${restaurant.city}</h4>
+        <p>${restaurant.description}</p>
+    </div>
+  </div>
+  `;
+
+const createLikeButtonTemplate = () => `
+  <button aria-label="like this restaurant" id="likeButton" class="like">
+    <i class="fa fa-heart-o" aria-hidden="true"></i>
+  </button>
+`;
+
+const createLikedButtonTemplate = () => `
+  <button aria-label="unlike this restaurant" id="likeButton" class="like">
+    <i class="fa fa-heart" aria-hidden="true"></i>
+  </button>
+`;
+
+export {
+  createRestaurantItemTemplate,
+  createRestaurantDetailTemplate,
+  createLikeButtonTemplate,
+  createLikedButtonTemplate,
+};
